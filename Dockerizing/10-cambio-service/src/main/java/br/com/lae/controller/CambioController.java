ca2fb81e19d1180e797552ba61a46663fb54f8c6
luciano-espindula <luc.aespindula@gmail.com>
2021-08-16 22:11:35 -0300

    Conteinerizando a Stack de Microsserviços com Docker e Docker Compose

---
+++ Dockerizing/10-cambio-service/src/main/java/br/com/lae/controller/CambioController.java
@@ -0,0 +1,55 @@
+package br.com.lae.controller;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lae.model.Cambio;
+import br.com.lae.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Cambio endpoint")
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+	
+	private Logger logger = org.slf4j.LoggerFactory.getLogger(CambioController.class);
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CambioRepository repository;
+	
+	// http://localhost:8000/cambio-service/5/USD/BRL
+	@Operation(summary = "Get cambio from currency")
+	@GetMapping(value = "/{amount}/{from}/{to}")
+	public Cambio getCambio(
+			@PathVariable("amount") BigDecimal amount,
+			@PathVariable("from") String from,
+			@PathVariable("to") String to) {
+		
+		logger.info("getCambio is called with -> {}, {} and {}", amount, from, to);
+
+		var cambio = repository.findByFromAndTo(from, to);
+		if (cambio == null) throw new RuntimeException("Currency Unsupported"); 
+		
+		var port = environment.getProperty("local.server.port");
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		cambio.setEnvironment(port);
+		
+		return cambio;
+	}
+
+}
